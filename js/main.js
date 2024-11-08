8a8223762eb4afff91887d1b28dfa47d239a0a22
Ziyoviddinfrontend <ziyoydinmuhtarov22@gmail.com>
2024-11-08 11:54:21 +0500

    country

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+const elBtn = document.querySelector('.btn');
+const elBody = document.querySelector('body')
+
+elBtn.addEventListener("click" , () => {   
+ elBody.classList.toggle('dark-mode')
+});
